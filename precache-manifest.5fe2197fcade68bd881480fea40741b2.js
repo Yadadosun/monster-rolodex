670fe69eceb7e08177deb8706ae84aba02a9e102
Dosun Kwon <dosunk@gmail.com>
2020-01-23 10:01:22 -0800

    Updates

---
+++ precache-manifest.5fe2197fcade68bd881480fea40741b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c4dcefa1897d316d400405cbc93727",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "1c17ec01a737689e12d3",
+    "url": "/monster-rolodex/static/css/main.b49e48d8.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c17ec01a737689e12d3",
+    "url": "/monster-rolodex/static/js/main.310e61b6.chunk.js"
+  },
+  {
+    "revision": "23fb47eb6f86565db1b0",
+    "url": "/monster-rolodex/static/js/runtime-main.47b0ddc3.js"
+  }
+]);
